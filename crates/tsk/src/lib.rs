f4e78b9eef0db468086fdd7587fbbb6e9f821d3e
Matthew Pfeiffer <mpfei@google.com>
2024-12-02 12:25:27 -0500

    Add more TSK wrappers

--- crates/tsk/src/lib.rs
+++ crates/tsk/src/lib.rs
@@ -1,8 +1,8 @@
 use std::marker::PhantomData;
-use std::ptr::NonNull;
+use std::ptr::{null_mut, NonNull};
 use std::{ffi::CStr, path::Path};
 
-use tsk_sys::TSK_FS_TYPE_ENUM_TSK_FS_TYPE_DETECT;
+use tsk_sys::{tsk_fs_dir_open, tsk_fs_file_open, TSK_FS_TYPE_ENUM_TSK_FS_TYPE_DETECT};
 
 pub fn get_tsk_version() -> String {
     let cstr = unsafe { CStr::from_ptr(tsk_sys::tsk_version_get_str()) };
@@ -32,9 +32,9 @@ impl TskPath {
     #[cfg(target_family = "unix")]
     fn from_path(path: &Path) -> Self {
         use std::os::unix::ffi::OsStrExt as _;
-        Self {
-            path: path.as_os_str().as_bytes().into(),
-        }
+        let mut path: Vec<u8> = path.as_os_str().as_bytes().into();
+        path.push(0);
+        Self { path }
     }
 
     #[cfg(target_os = "windows")]
@@ -85,9 +85,9 @@ impl TskImage {
         get_tsk_result(tsk_img_result).map(|inner| Self { inner })
     }
 
-    pub fn open_fs(&mut self) -> TskResult<TskFs> {
+    pub fn open_fs(&self) -> TskResult<TskFs> {
         let tsk_fs_result = unsafe {
-            tsk_sys::tsk_fs_open_img(self.inner.as_mut(), 0, TSK_FS_TYPE_ENUM_TSK_FS_TYPE_DETECT)
+            tsk_sys::tsk_fs_open_img(self.inner.as_ptr(), 0, TSK_FS_TYPE_ENUM_TSK_FS_TYPE_DETECT)
         };
         get_tsk_result(tsk_fs_result).map(|inner| TskFs {
             inner,
@@ -102,31 +102,120 @@ impl Drop for TskImage {
     }
 }
 
-pub struct TskFs<'image> {
+pub struct TskFs<'a> {
     pub(crate) inner: NonNull<tsk_sys::TSK_FS_INFO>,
-    _marker: PhantomData<&'image TskImage>,
+    _marker: PhantomData<&'a tsk_sys::TSK_FS_INFO>,
 }
 
-impl<'image> TskFs<'image> {
+impl<'a> TskFs<'a> {
     pub fn get_fs_type(&self) -> TskResult<String> {
-        let ty = unsafe { (*self.inner.as_ptr()).ftype };
+        let ty = unsafe { self.inner.as_ref().ftype };
         let name_ptr = unsafe { tsk_sys::tsk_fs_type_toname(ty) };
         get_tsk_result(name_ptr as _)
             .map(|non_null| unsafe { CStr::from_ptr(non_null.as_ptr()).to_bytes() })
             .map(|bytes| String::from_utf8_lossy(bytes).to_string())
     }
 
-    pub fn open_dir(path: &Path) -> TskResult<TskFsDir> {
+    pub fn open_dir(&self, path: &Path) -> TskResult<TskFsDir> {
+        let tsk_path = TskPath::from_path(path);
+        let result = unsafe { tsk_fs_dir_open(self.inner.as_ptr(), tsk_path.as_ptr()) };
+        get_tsk_result(result as _).map(|inner| TskFsDir {
+            inner,
+            _marker: PhantomData,
+        })
+    }
+
+    pub fn open_file(&self, path: &Path) -> TskResult<TskFsFile> {
+        let tsk_path = TskPath::from_path(path);
+        let result =
+            unsafe { tsk_fs_file_open(self.inner.as_ptr(), null_mut(), tsk_path.as_ptr()) };
+        get_tsk_result(result as _).map(|inner| TskFsFile {
+            inner,
+            _marker: PhantomData,
+        })
     }
 }
 
-impl<'image> Drop for TskFs<'image> {
+impl<'a> Drop for TskFs<'a> {
     fn drop(&mut self) {
         unsafe { tsk_sys::tsk_fs_close(self.inner.as_mut()) };
     }
 }
 
-pub struct TskFsDir {}
+pub struct TskFsDir<'a> {
+    pub(crate) inner: NonNull<tsk_sys::TSK_FS_DIR>,
+    _marker: PhantomData<&'a tsk_sys::TSK_FS_DIR>,
+}
+
+impl<'a> TskFsDir<'a> {
+    pub fn get_file(&mut self) -> TskFsFile {
+        let inner_ref = unsafe { self.inner.as_ref() };
+        NonNull::new(inner_ref.fs_file)
+            .map(|inner| TskFsFile {
+                inner,
+                _marker: PhantomData,
+            })
+            .expect("TSK_FS_DIR file is null")
+    }
+}
+
+pub struct TskFsFile<'a> {
+    pub(crate) inner: NonNull<tsk_sys::TSK_FS_FILE>,
+    _marker: PhantomData<&'a tsk_sys::TSK_FS_FILE>,
+}
+
+impl<'fs> TskFsFile<'fs> {
+    /// Returns the name of the file, or None if the file was opened by
+    /// metadata address.
+    pub fn get_name(&mut self) -> Option<TskFsName> {
+        let inner_ref = unsafe { self.inner.as_ref() };
+        NonNull::new(inner_ref.name).map(|inner| TskFsName {
+            inner,
+            _marker: PhantomData,
+        })
+    }
+
+    pub fn get_meta(&mut self) -> Option<TskFsMeta> {
+        let inner_ref = unsafe { self.inner.as_ref() };
+        NonNull::new(inner_ref.meta).map(|inner| TskFsMeta {
+            inner,
+            _marker: PhantomData,
+        })
+    }
+}
+
+pub struct TskFsName<'a> {
+    pub(crate) inner: NonNull<tsk_sys::TSK_FS_NAME>,
+    _marker: PhantomData<&'a tsk_sys::TSK_FS_NAME>,
+}
+
+impl<'a> TskFsName<'a> {
+    /// Returns the name of the file, or None if the name is invalid UTF-8.
+    pub fn get_name(&mut self) -> String {
+        unsafe {
+            CStr::from_ptr(self.inner.as_ref().name)
+                .to_string_lossy()
+                .to_string()
+        }
+    }
+}
+
+pub struct TskFsMeta<'a> {
+    pub(crate) inner: NonNull<tsk_sys::TSK_FS_META>,
+    _marker: PhantomData<&'a tsk_sys::TSK_FS_META>,
+}
+
+impl<'a> TskFsMeta<'a> {
+    pub fn addr(&self) -> u64 {
+        unsafe { self.inner.as_ref() }.addr
+    }
+    pub fn atime(&self) -> i64 {
+        unsafe { self.inner.as_ref() }.atime
+    }
+    pub fn atime_nano(&self) -> u32 {
+        unsafe { self.inner.as_ref() }.atime_nano
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -151,8 +240,17 @@ mod test {
             .expect("Failed to read test data");
         let mut tempfile = NamedTempFile::new().expect("Failed to open tempfile");
         tempfile.write(&ntfs_raw).expect("Failed to write tempfile");
-        let mut image = TskImage::open(tempfile.path()).expect("Failed to open ntfs image");
+        let image = TskImage::open(tempfile.path()).expect("Failed to open ntfs image");
         let fs = image.open_fs().expect("Failed to open NTFS FS");
-        eprintln!("{}", fs.get_fs_type().expect("Failed to get NTFS FS type"));
+        assert_eq!(fs.get_fs_type().unwrap(), "ntfs");
+        let mut root_f = fs
+            .open_file("/".as_ref())
+            .expect("Failed to open root file");
+        assert_eq!(root_f.get_meta().unwrap().addr(), 5);
+        let mut root_name = root_f.get_name().expect("Empty root name");
+        assert_eq!(root_name.get_name(), "");
+        let mut root_dir = fs.open_dir("/".as_ref()).expect("Failed to open root dir");
+        let mut root_f2 = root_dir.get_file();
+        assert_eq!(root_f2.get_meta().unwrap().addr(), 5);
     }
 }
